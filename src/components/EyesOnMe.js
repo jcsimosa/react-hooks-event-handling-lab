f760de32589b83c58390c19f0627d4fccb47505a
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-07-22 08:55:59 -0500

    commit tree 13cdbbeffa6537346f66a585266f6b86c624ae5f

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import react from "react";
+
+function EyeOnMe (){
+    
+    const text = () => {
+        console.log("Good!")
+    }
+    const text2 = () => {
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+    
+    <div>
+        <button onFocus={text} onBlur={text2}>Eyes on me</button>
+    </div>
+    )
+}
+export default EyeOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
+import react from "react";
 
 function Keypad (){
+    function handlePassword(e) {
+        console.log('Entering password...')
+    } 
+    
     return (
-        <div></div>
+        <input type="password" onChange={handlePassword}></input>
     )
 }
 
